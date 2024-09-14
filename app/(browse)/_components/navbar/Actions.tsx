de4ebc5dc976a8d6c2731e49b34a4224b95f1672
david emioma <ue22aac@herts.ac.uk>
2024-09-14 18:25:54 +0100

    Deployment issue #6

--- app/(browse)/_components/navbar/Actions.tsx
+++ app/(browse)/_components/navbar/Actions.tsx
@@ -13,14 +13,14 @@ const Actions = async () => {
   return (
     <div className="ml-4 flex items-center gap-3 md:gap-4 lg:ml-0">
       {!user && (
-        <SignInButton signUpFallbackRedirectUrl="/">
+        <SignInButton>
           <Button size="sm" variant="primary">
             Sign In
           </Button>
         </SignInButton>
       )}
 
-      {!!user && <UserButton signInUrl="/sign-in" />}
+      {!!user && <UserButton afterSignOutUrl="/" />}
 
       {!!user && (
         <CustomToolTip label="Dashboard" asChild side="bottom" align="center">
