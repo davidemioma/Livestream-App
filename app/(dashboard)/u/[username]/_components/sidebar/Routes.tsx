9538cefe0e5e14ab74c8124ff8db2f195f616c4c
david emioma <ue22aac@herts.ac.uk>
2024-09-16 14:24:26 +0100

    working on performance

--- app/(dashboard)/u/[username]/_components/sidebar/Routes.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/Routes.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { useUser } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import useCreatorSidebar from "@/hooks/use-creator-sidebar";
 import { Fullscreen, KeyRound, MessageSquare, Users } from "lucide-react";
@@ -51,25 +51,24 @@ const Routes = () => {
   }
 
   return (
-    <ul className="flex flex-col gap-2 px-2 pt-4 lg:pt-0">
+    <ul className="flex w-full flex-col gap-2 px-2 pt-4 lg:pt-0">
       {routes.map((route) => (
-        <Button
-          key={route.href}
+        <Link
           className={cn(
             "h-12 w-full",
             collapsed ? "justify-center" : "justify-start",
             pathname === route.href && "bg-accent",
+            buttonVariants({ variant: "ghost" }),
           )}
-          variant="ghost"
+          href={route.href}
+          key={route.href}
         >
-          <Link href={route.href}>
-            <div className="flex items-center gap-4">
-              <route.icon className="h-4 w-4" />
+          <div className="flex items-center gap-4">
+            <route.icon className="h-4 w-4" />
 
-              {!collapsed && <span>{route.name}</span>}
-            </div>
-          </Link>
-        </Button>
+            {!collapsed && <span>{route.name}</span>}
+          </div>
+        </Link>
       ))}
     </ul>
   );
